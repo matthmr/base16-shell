46392421b0e01fd77decb37f6286eebb17dbe4bf
mh <observermatth@gmail.com>
2024-09-30 01:08:39 -0300

    ad(base16-choose.sh): the old `sht` script

---
+++ base16-choose.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+this=$0
+this_dir=${this%/*}
+
+cd "${this_dir}/scripts"
+
+find . -maxdepth 1 -type f |\
+  fzfc --prompt "Choose theme: " --height=-100% |\
+  xargs -r "${this_dir}/../base16-apply.sh"
